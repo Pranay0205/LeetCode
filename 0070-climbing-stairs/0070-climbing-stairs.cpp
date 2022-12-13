660b42b09c391faeeca789b72e6be5354b75c499
Pranay Ghuge <35730341+Pranay0205@users.noreply.github.com>
2022-12-13 22:14:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (55.98%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    long dp[50] = {0};
+    int climbStairs(int n) {
+        if(n == 1){
+            return 1;
+        }
+        if(n == 2){
+            return 2;
+        }
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        return dp[n] = climbStairs(n-1) + climbStairs(n-2);
+    }
+};
