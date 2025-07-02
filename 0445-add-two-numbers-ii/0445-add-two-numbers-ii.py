fc786b86a324686e8369f242f5e3bee656e8d80f
Pranay Ghuge <35730341+Pranay0205@users.noreply.github.com>
2025-07-01 23:21:57 -0400

    Time: 13 ms (5.05%), Space: 17.9 MB (35.77%) - LeetHub

--- 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
+++ 0445-add-two-numbers-ii/0445-add-two-numbers-ii.py
@@ -6,14 +6,13 @@
 
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # Extract digits from first list
+
         list1 = ""
         head = l1
         while head != None:
             list1 += str(head.val)
             head = head.next
-        
-        # Extract digits from second list
+
         head = l2
         list2 = ""
         while head != None:
@@ -22,15 +21,13 @@ class Solution:
         
 
         
-        # Pad shorter string with leading zeros
         max_len = max(len(list1), len(list2))
         list1 = list1.zfill(max_len)
         list2 = list2.zfill(max_len)
         
         carry_over = 0
         res = ""
-        
-        # Process from right to left (least significant digit first)
+ 
         for i in range(max_len - 1, -1, -1):
             digit_sum = int(list1[i]) + int(list2[i]) + carry_over
             
@@ -40,17 +37,14 @@ class Solution:
             else:
                 carry_over = 0
                 res = str(digit_sum) + res
-        
-        # Handle final carry
+
         if carry_over:
             res = "1" + res
-        
-        # Reverse result since we need it in reverse order for the linked list
+
         res = res[::-1]
         
         print(res)
-        
-        # Build result linked list
+
         head = None       
         for num in res:
             head = ListNode(int(num), next=head)
